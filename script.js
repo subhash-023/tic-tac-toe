e3a23cdb7d7d47d414d2f513dee0c4fe6bb5639e
Subhash <subhash02305@gmail.com>
2024-12-30 13:27:37 +0000

    Provides object to control the game flow.
    The function GameController is created which applies game logic like:
    
    1. Switching active player after each round
    2. Checks for the winner every round

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ function GameBoard() {
     const rows = 3
     const columns = 3
 
-    for (i = 0; i < rows; i++){
+    for (i = 0; i < rows; i++) {        //creates 3x3 grid 
         gameBoard[i] = []
-        for(j = 0; j < columns; j++){
+        for (j = 0; j < columns; j++) {
             gameBoard[i].push(Cell())
         }
     }
 
     const getBoard = () => gameBoard
 
-    const addMarker = (row, column, player) => {
-        gameBoard[row][column].updateValue(player)
+    const addMarker = (row, column, player_marker) => {
+        gameBoard[row][column].updateValue(player_marker)
     }
 
     const printBoard = () => {
@@ -31,8 +31,8 @@ function GameBoard() {
 function Cell() {
     let value = 0;
 
-    const updateValue = (player) => {
-        value = player;
+    const updateValue = (player_marker) => {
+        value = player_marker;
     }
 
     const getValue = () => value;
@@ -42,3 +42,95 @@ function Cell() {
         getValue
     }
 }
+
+function GameController() {
+
+    const players = [
+        {
+            name: "playerOne",
+            marker: 1
+        },
+        {
+            name: "playerTwo",
+            marker: 2
+        }
+    ]
+
+    const boardInstance = GameBoard()
+
+    let activePlayer = players[0]
+
+    const switchActivePlayer = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0]
+    }
+
+    const getActivePlayer = () => activePlayer
+
+    const checkWinner = (board) => {
+
+        //check rows
+
+        for (let i = 0; i < 3; i++) {
+            if (board[i][0].getValue() !== 0 && board[i][0].getValue() === board[i][1].getValue() && board[i][1].getValue() === board[i][2].getValue())
+
+                return board[i][0]
+        }
+
+        //check columns
+
+        for (let i = 0; i < 3; i++) {
+            if (board[0][i].getValue() !== 0 && board[0][i].getValue() == board[1][i].getValue() && board[1][i].getValue() == board[2][i].getValue())
+                return board[0][i]
+        }
+
+        //check primary diagonal
+
+        if (board[0][0].getValue() !== 0 && board[0][0].getValue() ==  board[1][1].getValue() && board[1][1].getValue() == board[2][2].getValue()){
+            return board[0][0]
+        }
+
+        //check secondary diagonal
+
+        if (board[0][2].getValue() !== 0 && board[0][2].getValue() == board[1][1].getValue() && board[1][1].getValue() == board[2][0].getValue()){
+            return board[0][2]
+        }
+
+        return null
+    }
+
+    const playRound = (row, column) => {
+
+        const board = boardInstance.getBoard();
+        if (board[row][column].getValue() !== 0) {
+            console.log("Cell already occupied!");
+            return;
+        }
+
+        boardInstance.addMarker(row, column, getActivePlayer().marker)
+
+        switchActivePlayer()
+
+        boardInstance.printBoard()
+
+        console.log(checkWinner(board))
+    }
+
+    return {
+        getActivePlayer,
+        checkWinner,
+        playRound
+    }
+}
+
+// test = GameController()
+// test.playRound(1, 0)
+
+// test.playRound(0, 1)
+
+// test.playRound(1, 1)
+
+// test.playRound(0, 2)
+
+// test.playRound(1, 2)
+
+
